d4e14db41a00662b69db73837d803a46c82e7961
xiao.hy <xhyxzg@gmail.com>
2014-12-13 12:27:14 +0800

    bug fix fix some bug when use get request

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ define('__PUBLIC_DIR__', 'public');
 define('__ROOT__', dirname(__FILE__));
 define('__WEBROOT_DIR__', 'webroot');
 define('__WWW_ROOT__', __ROOT__ . DIRECTORY_SEPARATOR . __APP_DIR__ . DIRECTORY_SEPARATOR . __WEBROOT_DIR__ . DIRECTORY_SEPARATOR);
-define('__HOST_URL__', substr(__ROOT__, strlen($_SERVER['DOCUMENT_ROOT'])));
+
+$url = '/' . substr(__ROOT__, 1, strlen($_SERVER['DOCUMENT_ROOT']) - 1);
+
+define('__HOST_URL__', $url);
+unset($url);
+
 
 require(__ROOT__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'godle' . DIRECTORY_SEPARATOR . 'App' . DIRECTORY_SEPARATOR . 'App.class.php');
 
@@ -13,4 +18,4 @@ App::import('common', './', 'lib' . DIRECTORY_SEPARATOR . 'godle', '.php');
 App::import('Controller', 'Controller', 'lib' . DIRECTORY_SEPARATOR . 'godle');
 
 $conf = App::getConf();
-App::dispacher(substr($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'], strlen(__ROOT__)), $conf);
+App::dispacher(substr($_SERVER['DOCUMENT_ROOT'] . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), strlen(__ROOT__)), $conf);
